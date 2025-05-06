856871f0777a65dae099f4f5c883f347c8201bd4
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-06 17:26:06 -0300

    Valida dados do formulario de transferencia de valores

---
+++ app/Http/Requests/WithdrawRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WithdrawRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|uuid|exists:accounts,id|min:36|max:36',
+            'amount' => ['required' , 'numeric', 'regex:/^\d{1,13}(\.\d{1,2})?$/'], 
+        ];
+    }
+}
