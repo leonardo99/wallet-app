2ec9da1642dc1143a95b7fb0d1e6e6df52cac9ee
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-06 17:27:01 -0300

    Agrupa rotas de withdraw

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WithdrawController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Transactions
+    Route::group([
+        'prefix' => 'transaction', 
+        'as' => 'transaction.'
+    ], function () {
+        //Withdraws
+        Route::group(['prefix' => 'withdraw'], function() {
+            Route::get('/', [WithdrawController::class, 'create'])->name('withdraw.create');
+            Route::post('/', [WithdrawController::class, 'store'])->name('withdraw.store');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
