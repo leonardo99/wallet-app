147d6c94d3873be007411406c61c5ed64339f425
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-06 21:50:40 -0300

    Mostra saldo em conta e seta old

--- resources/views/withdraw/form.blade.php
+++ resources/views/withdraw/form.blade.php
@@ -11,6 +11,7 @@
                 <div class="border-b border-b-gray-100 px-6 pb-6">
                     <p class="font-bold">Informe o destinatário da transferência</p>
                     <p class="font-thin text-sm">Digite a chave da pessoa que vai receber o valor.</p>
+                    <p class="font-bold text-sm mt-1">Saldo {{ $balance }}</p>
                 </div>
                 <form action="{{route('transaction.withdraw.store')}}" method="post">
                     @csrf
@@ -26,7 +27,7 @@
                     
                         <div class="px-6 mt-3 flex flex-col w-full sm:w-1/2">
                             <label for="amount" class="text-sm font-medium text-gray-700 mb-1">Valor (R$)</label>
-                            <input type="text" id="amount" name="amount" value="{{old('id')}}"
+                            <input type="text" id="amount" name="amount" value="{{old('amount')}}"
                                 class="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm @error('amount') border-red-700 ring-red-700 focus:ring-red-700 focus:border-red-700 @enderror" />
                             @error('amount')
                                 <p class="text-xs font-thin font-medium text-red-700 my-1">{{ $message }}</p>                            
