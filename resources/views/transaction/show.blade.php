9373ba10690f4222f279de918a3c7e19f8324e4d
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-07 11:45:48 -0300

    Mostra dados da transação

--- resources/views/transaction/show.blade.php
+++ resources/views/transaction/show.blade.php
@@ -1,14 +1,38 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Transferir saldo
+            Detalhes da transação
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg py-6">
-                oi
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="space-y-3 bg-white overflow-hidden shadow-sm sm:rounded-lg py-6 flex flex-col items-center">
+                <div>
+                    <p><span class="font-bold">Id da transação:</span> {{ $transaction->id }}</p>
+                    <p class="text-gray-500 font-thin text-sm text-center">{{ $transaction->getDate() }}</p>
+                </div>
+                <p class="font-bold text-xl"> {{ $transaction->getAmount() }}</p>
+                <div class="flex justify-center items-center gap-1">
+                    <p class="font-thin">
+                        @if ($transaction->getStatusTransaction() === 'enviada')
+                        <x-uni-money-withdraw-o class="w-5 h-5" />  
+                        @elseif($transaction->getStatusTransaction() === 'recebida')
+                        <x-uni-money-insert-o class="w-5 h-5" />  
+                        @endif 
+                    </p>
+                    <p class="font-thin text-xl">
+                        Transferência {{ $transaction->getStatusTransaction() }}
+                    </p>
+                </div>
+                <p>
+                    @if ($transaction->getStatusTransaction() === 'enviada')
+                        {{ $transaction->receiverAccount->user->name }}
+                    @elseif($transaction->getStatusTransaction() === 'recebida')
+                        {{ $transaction->senderAccount->user->name }}
+                    @endif 
+                </p>
+                <button class="p-2 bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200 text-sm text-blue-500 rounded-sm cursor-pointer flex items-center gap-1"><x-bi-arrow-down-circle /> Devolver valor</button>
             </div>
         </div>
     </div>
