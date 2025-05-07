d195fff7b9ad02036e5f80c2653dc3dbf94ea862
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-07 17:58:54 -0300

    Adiciona modal para confirmar a devolução de valor

--- resources/views/transaction/show.blade.php
+++ resources/views/transaction/show.blade.php
@@ -30,10 +30,49 @@
                         {{ $transaction->receiverAccount->user->name }}
                     @elseif($transaction->getStatusTransaction() === 'recebida')
                         {{ $transaction->senderAccount->user->name }}
+                    @elseif($transaction->getStatusTransaction() === 'devolvida')
+                        {{ $transaction->senderAccount->user->name }}
                     @endif 
                 </p>
-                <button class="p-2 bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200 text-sm text-blue-500 rounded-sm cursor-pointer flex items-center gap-1"><x-bi-arrow-down-circle /> Devolver valor</button>
+                @can('update', $transaction)
+                    <button id="refund-value" class="p-2 bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200 text-sm text-blue-500 rounded-sm cursor-pointer flex items-center gap-1">
+                        <x-bi-arrow-down-circle /> Devolver valor
+                    </button>
+                    <form id="refund-form" method="POST" action="{{ route('transaction.refund', $transaction) }}" class="hidden">
+                        @csrf
+                    </form>
+                @endcan
             </div>
         </div>
     </div>
+    @push('js')
+        <script>
+            const transaction = @json($transaction);
+            document.querySelector("#refund-value").addEventListener('click', () => {
+                Swal.fire({
+                    title: 'Devolver valor',
+                    html: `
+                        <div class="text-left text-sm">
+                            <p><span class="font-bold">Saldo disponível:</span> {{ auth()->user()->account->getBalance() }}</p>
+                            <p><span class="font-bold">Você vai devolver:</span> {{ $transaction->getAmountFormated() }}</p>
+                            <p><span class="font-bold">Para:</span> {{ $transaction->senderAccount->user->name }}</p>
+                            <p><span class="font-bold">Chave de destino:</span> {{ $transaction->sender_account_id }}</p>
+                        </div>
+                    `,
+                    showCancelButton: true,
+                    confirmButtonText: 'Confirmar',
+                    cancelButtonText: 'Cancelar',
+                    customClass: {
+                        title: 'text-xl',
+                        confirmButton: 'bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200 text-sm text-blue-500',
+                        cancelButton: 'bg-grey-100 hover:bg-grey-200 focus:bg-grey-200 active:bg-grey-200 text-sm text-grey-500',
+                    },
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        document.getElementById('refund-form').submit();
+                    }
+                });
+            })
+        </script>
+    @endpush
 </x-app-layout>
